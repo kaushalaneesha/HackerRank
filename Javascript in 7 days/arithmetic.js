5d858e98ff2525d6760fd6077c6ac6bbe8f4db32
Aneesha Kaushal <aneeshakaushal1992@gmail.com>
2016-02-11 18:30:44 +0530

    Day2 javascript

---
+++ Javascript in 7 days/arithmetic.js
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/contests/7days-javascript/challenges/js-arithmetic-operators
+//Do not declare variables `a` and `b`.
+//Write your code below this line.
+var add = a+b
+var sub = a-b
+var mul = a*b
+var div = a/b
+a++;
+var inc = a
+b--;
+var dec = b
---
+++ Javascript in 7 days/variables.js
@@ -0,0 +1,3 @@
+//https://www.hackerrank.com/contests/7days-javascript/challenges/js-variables
+//Write your code below this line.
+var newVariable  =  "My First Variable"
